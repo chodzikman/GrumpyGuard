29f7e05ee18b90532f6e067403691ebc494d19f8
Michal Chodzikiewicz <chodzik@localhost.localdomain>
2015-02-03 20:16:57 +0100

    v0.1, working grumpyguard, very simple installation script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/sh
+
+cp grumpyg /usr/bin/grumpyg
+chmod +x /usr/bin/grumpyg
+mkdir /usr/share/grumpyg
+cp grumpywall.png /usr/share/grumpyg/grumpywall.png
+chmod 644         /usr/share/grumpyg/grumpywall.png
