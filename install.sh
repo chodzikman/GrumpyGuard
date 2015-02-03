c25d39b4a6aa2ff4168888090bd7c4341c2855e0
Michal Chodzikiewicz <chodzik@localhost.localdomain>
2015-02-03 21:31:32 +0100

    Script fixes

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/sh
 
 cp grumpyg /usr/bin/grumpyg
+cp i3lock /usr/bin/i3lock_grumpy
 chmod +x /usr/bin/grumpyg
+chmod +x /usr/bin/i3lock_grumpy
 mkdir /usr/share/grumpyg
 cp grumpywall.png /usr/share/grumpyg/grumpywall.png
 chmod 644         /usr/share/grumpyg/grumpywall.png
